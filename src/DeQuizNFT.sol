188fbcedbb4d0429d506a6ab9607819d2e5f0a4b
Max Ellisen <max@ezic.io>
2024-06-16 21:02:28 +0300

    Updated core contract to auto tokenId set during mint, created deployment script, updated vscode settings & solidity compiler

--- src/DeQuizNFT.sol
+++ src/DeQuizNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 /// @author ezic.io
 
@@ -20,37 +20,32 @@ contract DeQuizNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 private _tokenIdCounter;
     uint256 MAX_SUPPLY = 1_000; // Max supply is 1000;
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        address initialOwner
-    ) ERC721(name, symbol) Ownable(initialOwner) {
+    constructor(string memory name, string memory symbol, address initialOwner)
+        ERC721(name, symbol)
+        Ownable(initialOwner)
+    {
         // "deleted" values return to their default value, which is 0 for numbers;
         // Start counting from 1 to avoid security risks as suggested by Base Camp;
         _tokenIdCounter = 1;
     }
 
-    function safeMint(uint256 tokenId, string memory uri) public {
+    function safeMint(string memory uri) public {
         // Validate that max supply is not reached;
-        require(_tokenIdCounter <= MAX_SUPPLY, "I'm sorry we reached the cap");
+        require(_tokenIdCounter <= MAX_SUPPLY, "No more mints available");
         // Limit to 1 per wallet
         require(balanceOf(msg.sender) == 0, "Max Mint per wallet reached");
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, uri);
+        _safeMint(msg.sender, _tokenIdCounter);
+        _setTokenURI(_tokenIdCounter, uri);
         // Increment tokenId counter;
         _tokenIdCounter++;
     }
 
     // The following functions are overrides required by Solidity;
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
